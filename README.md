2e48d8257f1bdf739da666c30dc8d58504284b07
Natasha Giusti <natashagiusti@natashas-mbp.lan>
2019-03-09 23:04:18 -0800

    psuedo code updates

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Psychic-Game
+
+Guess what letter the computer's thinking!
+
+The computer will think of a random letter and you get 10 tries to guess what it is.
+
+Click a number on the keyboard.
+If it's right, you win!
+If it's wrong, guess again.
+
+The computer will keep track of your previous guesses. Good luck!
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,22 +8,46 @@ var guessesLeft= 9;
 var lettersGuessed = [];
 var computerAnswer = [];
 
-// generates computer answer
+// generates computer letter selection
 var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 computerAnswer.push(computerGuess);
+
+// selected letter in console log
 console.log(computerGuess[0]);
+// this was working but then I added something & broke it...
 
-// answer in console log
-console.log(answer);
 
-// game
+// Game
+// obtain user guess: when key is clicked, letter is stored
 document.onkeyup = function() {
     var userGuess = event.key;
     lettersGuessed.push(userGuess);
 
+// user guesses correctly: winner! wins increases, game resets (guessed letters cleared, guesses left resets)
     if (userGuess === computerGuess[0]) {
         wins++;
         guessesLeft = 9;
         lettersGuessed = [];
     }
+
+// user guesses incorrectly: guess' left decreases 
+    else (userGuess !== computerGuess[0]) {
+        guessesLeft--;
+        
+    // adds letters guessed to letters guessed
+        lettersGuessed.push(computerGuess);
+        document.getElementById('lettersGuessed').innerHTML = lettersGuessed;
+    }
+
+// user out of guesses: losses increases, guesses left resets
+    if (guessesLeft == 0) {
+        losses++;
+        guessesLeft = 9;
+    }
+
+// display date: number of wins, losses, attempts left, letters guessed
+    document.getElementById('wins').innerHTML = wins;
+    document.getElementById('losses').innerHTML = losses;
+    document.getElementById('guessesLeft').innerHTML = guessesLeft;
+
 }
